06da62149e4c6d7728749f53d3d7fcaf1f2c8483
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:48:33 -0300

    Update Operario.java

--- src/Operario.java
+++ src/Operario.java
@@ -3,5 +3,28 @@ public class Operario {
     private String nomeOperario;
     private String funcao;
 
-    // Getters e setters
+    // Getters e Setters
+    public int getIdOperario() {
+        return idOperario;
+    }
+
+    public void setIdOperario(int idOperario) {
+        this.idOperario = idOperario;
+    }
+
+    public String getNomeOperario() {
+        return nomeOperario;
+    }
+
+    public void setNomeOperario(String nomeOperario) {
+        this.nomeOperario = nomeOperario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
 }
