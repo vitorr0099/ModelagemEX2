7c3a0914b8321c01b8f46c8044019d7192f42d9a
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:59:11 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Sistema de Gerenciamento de Obras Iniciado.");
+        // Exemplo de chamadas de métodos para gerenciamento de projetos, engenheiros, operários, etc.
     }
 }
