d9d5d84dfae90efec44afe1dad90a5345bc888f7
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:35:45 -0300

    Update Projeto.java

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Projeto {
+public class Projeto {
+    private int idProjeto;
+    private String nomeProjeto;
+    private String local;
+    private Date dataInicio;
+    private Date dataTermino;
+
+    // Getters e setters
 }
