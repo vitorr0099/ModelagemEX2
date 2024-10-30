2e095a2e4e2840c8ea3b2b1f26e184a5c37aedad
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:49:12 -0300

    Update AlocacaoEngenheiro.java

--- src/AlocacaoEngenheiro.java
+++ src/AlocacaoEngenheiro.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class AlocacaoEngenheiro {
+public class AlocacaoEngenheiro {
+    private int idProjeto;
+    private int idEngenheiro;
+
+    // Getters e Setters
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(int idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public int getIdEngenheiro() {
+        return idEngenheiro;
+    }
+
+    public void setIdEngenheiro(int idEngenheiro) {
+        this.idEngenheiro = idEngenheiro;
+    }
 }
