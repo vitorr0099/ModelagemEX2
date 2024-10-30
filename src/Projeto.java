eab7e4ae26e1b665fcc186f7b194cb632d53f022
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:48:09 -0300

    Update Projeto.java

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Projeto {
     private int idProjeto;
     private String nomeProjeto;
@@ -5,5 +7,44 @@ public class Projeto {
     private Date dataInicio;
     private Date dataTermino;
 
-    // Getters e setters
+    // Getters e Setters
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(int idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(Date dataTermino) {
+        this.dataTermino = dataTermino;
+    }
 }
