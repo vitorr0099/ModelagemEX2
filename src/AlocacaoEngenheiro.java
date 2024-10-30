37bee73e1a066e9f477364ccb7797aeb9736d68f
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:57:14 -0300

    Update AlocacaoEngenheiro.java

--- src/AlocacaoEngenheiro.java
+++ src/AlocacaoEngenheiro.java
@@ -2,20 +2,16 @@ public class AlocacaoEngenheiro {
     private int idProjeto;
     private int idEngenheiro;
 
-    // Getters e Setters
+    public AlocacaoEngenheiro(int idProjeto, int idEngenheiro) {
+        this.idProjeto = idProjeto;
+        this.idEngenheiro = idEngenheiro;
+    }
+
     public int getIdProjeto() {
         return idProjeto;
     }
 
-    public void setIdProjeto(int idProjeto) {
-        this.idProjeto = idProjeto;
-    }
-
     public int getIdEngenheiro() {
         return idEngenheiro;
-    }
-
-    public void setIdEngenheiro(int idEngenheiro) {
-        this.idEngenheiro = idEngenheiro;
     }
 }
