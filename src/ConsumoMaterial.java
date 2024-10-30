496fdef320424bfba29d71e08823ebe5f587c48c
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:58:10 -0300

    Update ConsumoMaterial.java

--- src/ConsumoMaterial.java
+++ src/ConsumoMaterial.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ConsumoMaterial {
+public class ConsumoMaterial {
+    private int idProjeto;
+    private int idMaterial;
+
+    public ConsumoMaterial(int idProjeto, int idMaterial) {
+        this.idProjeto = idProjeto;
+        this.idMaterial = idMaterial;
+    }
+
+    public int getIdProjeto() {
+        return idProjeto;
+    }
+
+    public int getIdMaterial() {
+        return idMaterial;
+    }
 }
