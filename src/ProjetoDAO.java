c0b4b7da76effcdeea54a883088972b8e4993ee4
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:37:24 -0300

    Update ProjetoDAO.java

--- src/ProjetoDAO.java
+++ src/ProjetoDAO.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ProjetoDAO {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjetoDAO {
+    public void inserirProjeto(Projeto projeto) throws SQLException {
+        String sql = "INSERT INTO Projeto (Nome_Projeto, Local, Data_Inicio, Data_Termino) VALUES (?, ?, ?, ?)";
+        try (Connection conn = ConexaoBD.getConexao(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, projeto.getNomeProjeto());
+            stmt.setString(2, projeto.getLocal());
+            stmt.setDate(3, new java.sql.Date(projeto.getDataInicio().getTime()));
+            stmt.setDate(4, new java.sql.Date(projeto.getDataTermino().getTime()));
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<Projeto> listarProjetos() throws SQLException {
+        List<Projeto> projetos = new ArrayList<>();
+        String sql = "SELECT * FROM Projeto";
+        try (Connection conn = ConexaoBD.getConexao(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Projeto projeto = new Projeto();
+                projeto.setIdProjeto(rs.getInt("ID_Projeto"));
+                projeto.setNomeProjeto(rs.getString("Nome_Projeto"));
+                projeto.setLocal(rs.getString("Local"));
+                projeto.setDataInicio(rs.getDate("Data_Inicio"));
+                projeto.setDataTermino(rs.getDate("Data_Termino"));
+                projetos.add(projeto);
+            }
+        }
+        return projetos;
+    }
+
+    // Outros métodos CRUD para Projeto
 }
