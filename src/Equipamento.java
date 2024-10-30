95882691c21535caa5d1abaf0e99924f6cb86bb6
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:58:45 -0300

    Update Equipamento.java

--- src/Equipamento.java
+++ src/Equipamento.java
@@ -3,28 +3,21 @@ public class Equipamento {
     private String nomeEquipamento;
     private String tipo;
 
-    // Getters e Setters
-    public int getIdEquipamento() {
-        return idEquipamento;
+    public Equipamento(int idEquipamento, String nomeEquipamento, String tipo) {
+        this.idEquipamento = idEquipamento;
+        this.nomeEquipamento = nomeEquipamento;
+        this.tipo = tipo;
     }
 
-    public void setIdEquipamento(int idEquipamento) {
-        this.idEquipamento = idEquipamento;
+    public int getIdEquipamento() {
+        return idEquipamento;
     }
 
     public String getNomeEquipamento() {
         return nomeEquipamento;
     }
 
-    public void setNomeEquipamento(String nomeEquipamento) {
-        this.nomeEquipamento = nomeEquipamento;
-    }
-
     public String getTipo() {
         return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
     }
 }
