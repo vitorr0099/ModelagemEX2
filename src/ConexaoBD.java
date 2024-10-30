45eab79647842b0e6163b277ad74048be69a80b8
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:36:38 -0300

    Update ConexaoBD.java

--- src/ConexaoBD.java
+++ src/ConexaoBD.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ConexaoBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+    private static Connection conexao;
+
+    private ConexaoBD() {}
+
+    public static Connection getConexao() throws SQLException {
+        if (conexao == null || conexao.isClosed()) {
+            conexao = DriverManager.getConnection("jdbc:url_do_banco_de_dados", "usuario", "senha");
+        }
+        return conexao;
+    }
 }
