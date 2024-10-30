95fdd9f9f08a834c594c58fc3fc00270a0840bfe
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:59:23 -0300

    Update Material.java

--- src/Material.java
+++ src/Material.java
@@ -3,28 +3,21 @@ public class Material {
     private String nomeMaterial;
     private int quantidade;
 
-    // Getters e Setters
-    public int getIdMaterial() {
-        return idMaterial;
+    public Material(int idMaterial, String nomeMaterial, int quantidade) {
+        this.idMaterial = idMaterial;
+        this.nomeMaterial = nomeMaterial;
+        this.quantidade = quantidade;
     }
 
-    public void setIdMaterial(int idMaterial) {
-        this.idMaterial = idMaterial;
+    public int getIdMaterial() {
+        return idMaterial;
     }
 
     public String getNomeMaterial() {
         return nomeMaterial;
     }
 
-    public void setNomeMaterial(String nomeMaterial) {
-        this.nomeMaterial = nomeMaterial;
-    }
-
     public int getQuantidade() {
         return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
     }
 }
